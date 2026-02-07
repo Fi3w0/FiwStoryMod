d7e60322ae7edabbfa52d21472a636df769a0a7c
Fi3w0 <fi3w0@github.com>
2026-02-07 04:50:49 +0100

    Add mod core infrastructure
    - Main mod class with initialization
    - Client-side mod class for rendering
    - fabric.mod.json with mod metadata
    - Basic mod structure and entry points

---
+++ src/main/java/com/fiw/fiwstory/FiwstoryMod.java
@@ -0,0 +1,102 @@
+package com.fiw.fiwstory;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.dimension.DimensionOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fiw.fiwstory.command.BindCommand;
+import com.fiw.fiwstory.command.CorruptionCommand;
+import com.fiw.fiwstory.command.ImmunityCommand;
+import com.fiw.fiwstory.command.VoidCommand;
+import com.fiw.fiwstory.effect.ModStatusEffects;
+import com.fiw.fiwstory.event.AmethystDropEvent;
+import com.fiw.fiwstory.event.CorruptionPreventionEvent;
+import com.fiw.fiwstory.event.CorruptionTrackingEvent;
+import com.fiw.fiwstory.event.ImmunityWhisperEvent;
+import com.fiw.fiwstory.event.ModEvents;
+import com.fiw.fiwstory.event.PhilosopherStoneEvents;
+import com.fiw.fiwstory.event.SoulboundDeathHandler;
+import com.fiw.fiwstory.item.ModItems;
+import com.fiw.fiwstory.particles.VoidParticles;
+
+public class FiwstoryMod implements ModInitializer {
+	public static final String MOD_ID = "fiwstory";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final ItemGroup FIWSTORY_GROUP = FabricItemGroup.builder()
+			.icon(() -> new ItemStack(ModItems.CORRUPTED_CRYSTAL))
+			.displayName(Text.translatable("itemgroup.fiwstory.main"))
+			.entries((context, entries) -> {
+				entries.add(ModItems.CORRUPTED_CRYSTAL);
+				entries.add(ModItems.CURSED_SPEAR_OF_FI3W0);
+				entries.add(ModItems.PHARAOH_SCARAB_ARTIFACT);
+				entries.add(ModItems.CHAOS_GEM_ARTIFACT);
+				entries.add(ModItems.BLOOD_GEM_ARTIFACT);
+				entries.add(ModItems.PHARAOH_RING_ARTIFACT);
+				entries.add(ModItems.TEMPORAL_STRUCTURE_ARTIFACT);
+				entries.add(ModItems.FI3W0_GLASSES);
+				entries.add(ModItems.PHILOSOPHER_STONE_ARTIFACT);
+				entries.add(ModItems.PHILOSOPHER_STONE_UPGRADED_ARTIFACT);
+				entries.add(ModItems.FALLEN_GOD_HEART_ARTIFACT);
+				entries.add(ModItems.PURE_CRYSTAL);
+				entries.add(ModItems.PURE_MIX);
+				entries.add(ModItems.HEALING_RUNE);
+				entries.add(ModItems.PHARAOH_DAGGER_ARTIFACT);
+				entries.add(ModItems.ESCARABAJO_ARTIFACT);
+				entries.add(ModItems.ESPADA_CAOS_ARTIFACT);
+				entries.add(ModItems.TIMELESS_BLADE_ARTIFACT);
+				entries.add(ModItems.DIVINE_BLOOD);
+			})
+			.build();
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Initializing Fiw Story Mod v1.3.21");
+
+		ModItems.registerModItems();
+		Registry.register(Registries.ITEM_GROUP, new Identifier(MOD_ID, "main"), FIWSTORY_GROUP);
+		
+		// Registrar efectos de estado
+		ModStatusEffects.registerStatusEffects();
+		
+		ModEvents.registerServerEvents();
+		AmethystDropEvent.register();
+		PhilosopherStoneEvents.registerEvents();
+		CorruptionTrackingEvent.registerEvents();
+		ImmunityWhisperEvent.registerEvents();
+		CorruptionPreventionEvent.registerEvents();
+		
+		// Registrar sistema Soulbound
+		SoulboundDeathHandler.registerEvents();
+		
+		// Registrar sistema de Void
+		// VoidEvents.register() se llama automáticamente a través de los eventos
+		
+		// Registrar partículas del Void
+		VoidParticles.register();
+		
+		// Registrar comandos
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			ImmunityCommand.register(dispatcher, registryAccess, environment);
+			BindCommand.register(dispatcher, registryAccess, environment);
+			CorruptionCommand.register(dispatcher, registryAccess, environment);
+			VoidCommand.register(dispatcher, registryAccess, environment);
+		});
+
+		LOGGER.info("Fiw Story Mod initialized successfully!");
+		LOGGER.info("Timeless Void dimension system ready");
+		LOGGER.info("Commands: /v enter, /v leave, /v whitelist");
+		LOGGER.info("Timeless Blade can access Void (10s cooldown)");
+	}
+}
---
+++ src/main/java/com/fiw/fiwstory/FiwstoryModClient.java
@@ -0,0 +1,27 @@
+package com.fiw.fiwstory;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.fabric.api.client.rendering.v1.ArmorRenderer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+
+import com.fiw.fiwstory.client.Fi3w0GlassesModelLayers;
+import com.fiw.fiwstory.client.Fi3w0GlassesRenderer;
+import com.fiw.fiwstory.event.ModEvents;
+import com.fiw.fiwstory.item.ModItems;
+
+@Environment(EnvType.CLIENT)
+public class FiwstoryModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		ModEvents.registerClientEvents();
+		
+		// Register Fi3w0 Glasses model layer
+		EntityModelLayerRegistry.registerModelLayer(Fi3w0GlassesModelLayers.FI3W0_GLASSES, 
+			Fi3w0GlassesModelLayers::getFi3w0GlassesTexturedModelData);
+		
+		// Register Fi3w0 Glasses armor renderer
+		ArmorRenderer.register(new Fi3w0GlassesRenderer(), ModItems.FI3W0_GLASSES);
+	}
+}
